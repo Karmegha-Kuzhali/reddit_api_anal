31d2a21c3959274ea555c09c1ac23d5bae87cd02
Karmegha-Kuzhali <piperofclouds@gmail.com>
2025-05-13 20:31:35 +0530

    Add files via upload

--- scraper.py
+++ scraper.py
@@ -7,11 +7,21 @@
 import praw
 from dotenv import load_dotenv
 import os
+import csv
+from fns import get_body
 load_dotenv()
 reddit=praw.Reddit(client_id=os.getenv('client_id'),client_secret=os.getenv('client_secret'),user_agent=os.getenv('user_agent'),username=os.getenv('username'),password=os.getenv('password'))
-me=reddit.redditor(os.getenv('outuser'))
-l=[]
-for i in me.comments.hot():
-    l.append((i.submission.title,i.submission.author))
-print(l)
+usr=reddit.redditor(os.getenv('outuser'))
+sub=reddit.subreddit(os.getenv('subname'))
+d={}
+for subs in sub.new(limit=2000):
+    if subs.title in d:
+        d[subs.title]=max(d[subs.title],subs.upvote_ratio)
+    else:
+        d[subs.title]=subs.upvote_ratio
 
+f=open(sub.display_name+".csv",'w')
+cw=csv.writer(f)
+for i in d:
+    cw.writerow([i,d[i]])
+f.close()
